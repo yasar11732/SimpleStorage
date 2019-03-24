c26f462b810f0762943ad42bc42186c571de3b69
Yaşar Arabacı <yasar11732@gmail.com>
2019-03-24 12:16:05 +0300

    Add project files.

---
+++ SimpleStorage.Tests/SimpleStorageTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System.IO;
+
+namespace SimpleStorage.Tests
+{
+    [TestFixture]
+    public class SimpleStorageTests
+    {
+        [Test]
+        public void Database_ThrowsUnauthorizedAccessException()
+        {
+            string filename = "non-existent-path";
+            Assert.That(() => new Database(filename),
+                Throws.InstanceOf(typeof(System.UnauthorizedAccessException)));
+        }
+        [Test]
+        public void Database_CanInstantiateWithGivenPath()
+        {
+            string filename = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Database d = new Database(filename);
+            Assert.That(d.data_directory, Is.EqualTo(filename));
+            Assert.That(d.data_directory, Does.Exist);
+            Directory.Delete(d.data_directory, true);
+        }
+
+        [Test]
+        public void Database_CanInstantiateWithNullPath()
+        {
+            Database d = new Database(null);
+            Assert.That(d.data_directory, Is.Not.Null);
+        }
+
+    }
+}
---
+++ SimpleStorage/SimpleStorage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SimpleStorage
+{
+    public class Database
+    {
+        private string _data_directory = null;
+        public string data_directory
+        {
+            get
+            {
+                return _data_directory;
+            }
+        }
+
+        public Database(string data_directory)
+        {
+            if(data_directory == null)
+            {
+                data_directory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+                Directory.CreateDirectory(data_directory);
+            }
+
+            _data_directory = data_directory;
+            Directory.CreateDirectory(_data_directory);
+        }
+
+        public void Put(string collection, UInt64 key, byte[] data)
+        {
+
+        }
+
+        public byte[] Get(string collection, UInt64 key)
+        {
+            return null;
+        }
+
+        public void Remove(string collection, UInt64 key)
+        {
+
+        }
+    }
+}
